a4fb963db8b160ba6c9aef63478ba8e70354e0f5
asdfptj <taejunb70@gmail.com>
2023-06-12 16:56:14 +0900

    first commit

---
+++ src/main/java/com/mybatis/MB_Board/MbBoardApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis.MB_Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybatis/MB_Board/MbBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis.MB_Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
